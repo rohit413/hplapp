7f9fc450cdfcc26da57a4c5eb017f75a0a420e1f
dksingh413 <dksinghmern@gmail.com>
2026-03-16 23:15:56 +0530

    fix: remove Google Fonts dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 
 import { Header } from "@/components/header";
 import Footer from "@/components/Footer";
 import Widget from "@/components/Widget";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "HPLA-High Polymer Labs Limited",
@@ -39,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <ClientErrorBoundary>
           <Header />
